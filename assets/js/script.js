2760456f5b653c90761785d8fe42b25d4662eafb
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-03-10 18:24:57 +0100

    carosello-da-fixare

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 
-  document.querySelector('#carousel').carousel({
-    interval: 1000
-  });
+var carousel = document.querySelector('#carousel');
+var prevArrow = document.querySelector('.carousel-control-prev');
+var nextArrow = document.querySelector('.carousel-control-next');
+
+var carouselInstance = new bootstrap.Carousel(carousel);
+
+prevArrow.addEventListener('click', function() {
+  carouselInstance.prev();
+});
+
+nextArrow.addEventListener('click', function() {
+  carouselInstance.next();
+});
